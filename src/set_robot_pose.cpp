bfc736802f704f8b2f17d905a18f868baf864b47
Juan Carlos Garcia <jc.garsan@gmail.com>
2017-02-22 12:32:22 +0100

    Robot pose dialog modified

--- src/set_robot_pose.cpp
+++ src/set_robot_pose.cpp
@@ -13,17 +13,21 @@
 
 
 #include "../include/merbots_gui/set_robot_pose.h"
-//#include "../include/merbots_gui/ui_set_robot_pose.h"
 #include "ui_set_robot_pose.h"
 
+#include <ros/package.h>
 
-SetRobotPoseDlg::SetRobotPoseDlg(QWidget *parent) :
+SetRobotPoseDlg::SetRobotPoseDlg(ros::NodeHandle *nodeHdl, QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::SetRobotPoseDlg)
+    ui(new Ui::SetRobotPoseDlg),
+    _nh(nodeHdl)
 {
     ui->setupUi(this);
-    ui->pushButton->setText("Done");
-   	QObject::connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(testButtonClicked()));
+   	QObject::connect(ui->getMarkerPoseButton, SIGNAL(clicked()), this, SLOT(testButtonClicked()));
+   	QObject::connect(ui->cancelButton, SIGNAL(clicked()), this, SLOT(close()));
+
+	sub_tf	= _nh->subscribe<auv_msgs::NavSts>("/tf", 1, &SetRobotPoseDlg::get500MarkerPose, this); 
+	//sub_ry	=  nh->subscribe<auv_msgs::NavSts>(ui.g500TopicOdometry->text().toUtf8().constData(), 1, &MainWindow::g500OdometryCallback, this); 
 
 }
 
@@ -34,5 +38,15 @@ SetRobotPoseDlg::~SetRobotPoseDlg()
 
 void SetRobotPoseDlg::testButtonClicked()
 {
-	qDebug() << "Done button";
+	qDebug() << "Getting the Marker Pose...";
+}
+
+void SetRobotPoseDlg::get500MarkerPose(const auv_msgs::NavSts::ConstPtr& g500OdometryInfo)
+{
+	ui->xValue->setText(QString::number(g500OdometryInfo->position.north));
+	ui->yValue->setText(QString::number(g500OdometryInfo->position.north));
+	ui->zValue->setText(QString::number(g500OdometryInfo->position.north));
+	ui->rollValue->setText(QString::number(g500OdometryInfo->position.north));
+	ui->pitchValue->setText(QString::number(g500OdometryInfo->position.north));
+	ui->yawValue->setText(QString::number(g500OdometryInfo->position.north));
 }
